8ba6e096637dc22dc79e42ed414880fd9ed41d2f
Shourtu1 <123448767+Shourtu1@users.noreply.github.com>
2023-02-09 14:08:57 +0000

    modified:   bettervanilla/src/routes/about/+page.svelte

--- bettervanilla/src/routes/about/+page.svelte
+++ bettervanilla/src/routes/about/+page.svelte
@@ -5,7 +5,7 @@
 
 <div style="padding-top: 4rem;">
 <center>
-    <h1><kbd>WEBSITE IS UNDER CONSTRUCTION!!!</kbd></h1>
+    <h1><kbd>Hi IS UNDER CONSTRUCTION!!!</kbd></h1>
     <small>Go back to the <a href="/">homepage</a></small>
 </center>
 </div>
